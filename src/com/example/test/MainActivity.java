44f614c85364b9f2f5397bc2061d2ad5f92a02c1
unknown <5220170194@CODE-077.vdi.pri>
2019-11-18 18:29:44 +0800

    commit

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.test;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends Activity {
 
@@ -19,5 +21,16 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
